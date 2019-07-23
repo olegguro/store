f6c0356e29b1fdb78143d28107085d8dc88d9859
Oleg Guro <ooleg6334@gmail.com>
2019-07-23 15:43:42 +0300

    Added paramentrs to classes Book and Film

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,18 @@
 class Book < Product
+  attr_reader :title, :genre, :author
+
+  def initialize(params)
+    # вызовем конструктор класса-родителя Product,
+    # он заполнит переменные экземпляра @price и @amount,
+    super
+
+    @title = params[:title]
+    @genre = params[:genre]
+    @author = params[:author]
+  end
+
+  # Метод to_s возвращает описанием и дергает родительский метод to_s, чтобы склеить эту строку с ценой и остатком.
+  def to_s
+    "Книга «#{@title}», #{@genre}, автор — #{@author}, #{super}"
+  end
 end
